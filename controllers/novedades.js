8e49017dc1b436aafc16f29c9ae0db03a1de3bb2
Juan Rafael Saavedra Sanguinetti <juan.rafael.saavedra@fing.edu.uy>
2019-02-11 13:37:25 -0300

    Carrousel in Novedades, Routing for each novedad, header black when novedad selevted

--- controllers/novedades.js
+++ controllers/novedades.js
@@ -4,21 +4,21 @@ app.controller('NovedadesController', function($scope,$location) {
 	{
 		id:1,
 		imageUrl : "images/img_1.jpg",
-		title: "Lorem Ipsum Dolor Sit Amet",
+		title: "Novedad 1",
 		date: "20/10/2018",
 		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolores culpa qui aliquam placeat nobis veritatis tempora natus rerum obcaecati."
 	},
 	{
 		id:2,
 		imageUrl : "images/img_2.jpg",
-		title: "Lorem Ipsum Dolor Sit Amet",
+		title: "Novedad 2",
 		date: "11/6/2018",
 		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolores culpa qui aliquam placeat nobis veritatis tempora natus rerum obcaecati."
 	},
 	{
 		id:3,
 		imageUrl : "images/img_3.jpg",
-		title: "Lorem Ipsum Dolor Sit Amet",
+		title: "Novedad 3",
 		date: "04/01/2018",
 		description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolores culpa qui aliquam placeat nobis veritatis tempora natus rerum obcaecati."
 	},
@@ -26,10 +26,14 @@ app.controller('NovedadesController', function($scope,$location) {
   $scope.showNovedad = false;
   $scope.NovedadSelected = undefined;
   $scope.init = function () {
-    var paramValue = $location.search().nov;
-    if (paramValue != undefined){
+  	url = window.location.href ;
+	expresion = /nov=(\d+)/;
+	novMatch = url.match(expresion)
+    if (novMatch != null){
+		novId = novMatch[1];
     	$scope.showNovedad = true;
-    	$scope.NovedadSelected = $scope.novedades[paramValue];
+    	$scope.novedades = [$scope.novedades[novId]];
+    	document.getElementById("menuBar").style.backgroundColor  = "black";
     }
    }
    $scope.init();
